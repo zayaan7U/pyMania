7715c3769c4bd42fd689527708c8c1b96ac2ebf2
zayaan7U <zayaanullah45@gmail.com>
2026-03-06 12:49:05 +0000

    committed

--- main/constants.py
+++ main/constants.py
@@ -3,17 +3,17 @@ from functions import scale_image
 
 pygame.init()
 
-GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
-TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+GRASS = scale_image(pygame.image.load("main/imgs/grass.jpg"),2.5)
+TRACK = scale_image(pygame.image.load("main/imgs/track.png"),0.9)
 
-TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+TRACK_BORDER = scale_image(pygame.image.load("main/imgs/track-border.png",), 0.9)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
-FINISH = pygame.image.load("imgs/finish.png")
+FINISH = pygame.image.load("main/imgs/finish.png")
 FINISH_MASK = pygame.mask.from_surface(FINISH)
 FINISH_POSITION = (130,250)
 
-CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+CAR = scale_image(pygame.image.load("main/imgs/red-car.png"),0.55) #0.6 seemed too big and 0.5 seemed too small
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 FPS = 60

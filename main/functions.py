b4f3c3a69455c02ba459e2639dee95ee8bd1ff3b
zayaan7U <zayaanullah45@gmail.com>
2026-03-08 22:51:01 +0000

    Remove unused blit_text_center function
    used for title screen but i flopped

--- main/functions.py
+++ main/functions.py
@@ -9,15 +9,3 @@ def blit_rotate_center(win, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center) # corrects image placement for car
     win.blit(rotated_image, new_rect.topleft)
-
-def blit_text_center(win, font, text):
-    render = font.render(text, True, (200,200,200))
-
-    win.blit
-    (
-        render,
-        (
-            win.get_width()/2 - render.get_width()/2,
-            win.get_height()/2 - render.get_height()/2
-        )
-    )
